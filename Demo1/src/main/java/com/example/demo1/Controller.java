ac505062ec2034e7611d9483717a74609727d9ca
tashapav <np76863@gmail.com>
2023-05-23 08:29:52 +0300

    prometheus

--- Demo1/src/main/java/com/example/demo1/Controller.java
+++ Demo1/src/main/java/com/example/demo1/Controller.java
@@ -5,16 +5,21 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 import java.lang.Math;
 
 @RestController
 @Slf4j
 public class Controller {
     private final RabbitTemplate rabbitTemplate;
+    private final Counter requests;
 
     @Autowired
-    Controller(RabbitTemplate rabbitTemplate) {
+    Controller(RabbitTemplate rabbitTemplate, MeterRegistry meterRegistry) {
         this.rabbitTemplate = rabbitTemplate;
+        requests = meterRegistry.counter("counter");
     }
 
     @PostMapping("/randomTimesMakeSleep")
@@ -22,6 +27,7 @@ public class Controller {
         log.info("1");
         for (int i = 0; i < times; ++i) {
             rabbitTemplate.convertAndSend(SomeConfiguration1.NAME1, (int) (Math.random() * 7));
+            requests.increment();
         }
     }
 }
--- demo2/src/main/java/com/example/demo2/Worker.java
+++ demo2/src/main/java/com/example/demo2/Worker.java
@@ -7,21 +7,31 @@ import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 
 @Service
 @Slf4j
 public class Worker {
     private final RabbitTemplate rabbitTemplate;
+    private final Counter goodResponse;
+    private final Counter error;
 
-    Worker(RabbitTemplate rabbitTemplate)  { this.rabbitTemplate = rabbitTemplate; }
+    Worker(RabbitTemplate rabbitTemplate, MeterRegistry meterRegistry)  {
+        this.rabbitTemplate = rabbitTemplate;
+        goodResponse = meterRegistry.counter("goodresponse");
+        error = meterRegistry.counter("error");
+    }
     @RabbitListener(queues = {SomeConfiguration2.NAME1})
     public void workerdo(Integer sleepCount, MessageHeaders headers, @Header(AmqpHeaders.CONSUMER_QUEUE) String queueName) throws InterruptedException {
         try {
             log.info("2");
             Thread.sleep(sleepCount);
             rabbitTemplate.convertAndSend(SomeConfiguration2.NAME2, sleepCount);
+            goodResponse.increment();
         } catch (Exception e) {
+            error.increment();
             log.info("Exception in worker");
         }
     }
